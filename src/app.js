c04abe9bcacc97f85afcca52e70f70a9171d7040
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-14 15:17:50 +0000

    [ADD] file app

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//app
+const express = require('express')
+const pino = require("pino-http")()
+const app = express()
+app.use(pino)
+module.exports = app
+//endpoints
--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
+// serever
 require('dotenv').config()
-const express = require('express')
-const pino = require("pino-http")()
 const logger = require('pino')()
-const app = express()
-app.use(pino)
+const app = require('./app')
 const port = process.env.PORT
 app.listen( port, ()=> {
     logger.info(`Server is listening on port ${port}`)
