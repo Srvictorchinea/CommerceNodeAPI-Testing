2cedb0cdad904e318034bfab562dea36a2e90e88
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-16 15:05:57 +0000

    [ADD] endpoints full apirest

--- src/app.js
+++ src/app.js
@@ -9,14 +9,44 @@ const router = express.Router()
 const users = []
 
 router.post('/users', (req, res) => {
-    const {name, address, age} = req.body
-    users.push({name, address, age})
+    const {name, address, age, uid} = req.body
+    users.push({name, address, age, uid})
     return res.status(201).send(users)
 })
 
 router.get('/users', (req, res) => {
     return res.status(200).send(users)
 })
+
+router.get('/users/:uid', (req, res) => {
+    const {uid} = req.params
+    const user = users.find(({uid: userUid}) => userUid === uid)
+
+    return res.status(200).send(user)
+})
+
+router.put('/users/:uid', (req, res) => {
+    const {name, address, age} = req.body
+    const {uid} = req.params
+
+    const usersUpdate = users.map((user) => {
+        if (user.uid === uid) {
+            return { ...user, name, address, age }
+        }
+
+        return user
+    })
+    return res.status(200).send(usersUpdate)
+})
+
+
+router.delete('/users/:uid', (req, res) => {
+    const {uid} = req.params
+    const user = users.filter(({uid: userUid}) => userUid === uid)
+
+    return res.status(200).send(user)
+})
+
 
 app.use(router)
 
